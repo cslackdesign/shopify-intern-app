ffd7a8f4be79d87e186fd0498e2b6647e072da39
Cassie Slack <cslackdesign@gmail.com>
2013-11-13 20:44:19 -0500

    Fixed all the things :')

--- js/work-div.js
+++ js/work-div.js
@@ -1,123 +1,63 @@
 $(document).ready(function(){
+  var openWork = 'work';
 
-  // HIDE
-  $('#dropkick').hide();
-  $('#iceland').hide();
-  $('#collective').hide();
-  $('#web-1').hide();
-  $('#intranet').hide();
-  $('#web-2').hide();
+  $('.work-div').hide();
 
-//SHOW ON CLICK
-  $('#dropkick-button').click(function(){
-      $('#work').fadeOut(400);
-      $('#dropkick').fadeIn(800);
+  $('.work-btn').click(function () {
+    var id = $(this).data('id');
+    openWork = id;
+    $('#work').fadeOut(400);
+    $('#' + id).fadeIn({
+      duration: 800,
+      start: function () {
+        $('#work-wrap').height($('#' + id).outerHeight());
+      }
+    });
   });
 
-  $('#iceland-button').click(function(){
-      $('#work').fadeOut(400);
-      $('#iceland').fadeIn(800);
+  $('.close-btn').click(function () {
+    $(this).parents('.work-div').fadeOut(400);
+    openWork = 'work';
+    $('#work').fadeIn({
+      duration: 400,
+      start: function () {
+        $('#work-wrap').height($('#work').outerHeight());
+      }
+    });
   });
 
-  $('#collective-button').click(function(){
-      $('#work').fadeOut(400);
-      $('#collective').show();
+  $('.next-btn').click(function () {
+    var $workDiv = $(this).parents('.work-div');
+    var $nextDiv = $(this).parents('.work-div').next('.work-div');
+    var id = $nextDiv.attr('id');
+    openWork = id;
+
+    $workDiv.fadeOut(400);
+    $nextDiv.fadeIn({
+      duration: 400,
+      start: function () {
+        $('#work-wrap').height($('#' + id).outerHeight());
+      }
+    });
   });
 
-  $('#web-1-button').click(function(){
-      $('#work').fadeOut(400);
-      $('#web-1').show();
+  $('.prev-btn').click(function() {
+    var $workDiv = $(this).parents('.work-div');
+    var $prevDiv = $(this).parents('.work-div').prev('.work-div');
+    var id = $prevDiv.attr('id');
+    openWork = id;
+
+    $workDiv.fadeOut(400);
+    $prevDiv.fadeIn({
+      duration: 400,
+      start: function () {
+        $('#work-wrap').height($('#' + id).outerHeight());
+      }
+    });
   });
 
-  $('#intranet-button').click(function(){
-      $('#work').fadeOut(400);
-      $('#intranet').fadeIn(800);
+  $(window).on('resize', function () {
+    $('#work-wrap').height($('#' + openWork).outerHeight());
   });
 
-  $('#web-2-button').click(function(){
-      $('#work').fadeOut(400);
-      $('#web-2').fadeIn(800);
-  });
-//CLOSE BUTTONS
-  $('#close-dropkick').click(function(){
-      $('#dropkick').fadeOut(400);
-      $('#work').fadeIn(800);
-  });
-
-  $('#close-iceland').click(function(){
-      $('#dropkick').fadeOut(400);
-      $('#work').fadeIn(800);
-  });
-
-  $('#close-collective').click(function(){
-      $('#collective').fadeOut(400);
-      $('#work').fadeIn(800);
-  });
-
-  $('#close-web-1').click(function(){
-      $('#web-1').fadeOut(400);
-      $('#work').fadeIn(800);
-  });
-
-  $('#close-intranet').click(function(){
-      $('#intranet').fadeOut(400);
-      $('#work').fadeIn(800);
-  });
-
-  $('#close-web-2').click(function(){
-      $('#web-2').fadeOut(400);
-      $('#work').fadeIn(800);
-  });
-//NEXT
-
-  $('#next-iceland').click(function(){
-      $('#dropkick').fadeOut(400);
-      $('#iceland').fadeIn(800);
-  });
-
-  $('#next-collective').click(function(){
-      $('#iceland').fadeOut(400);
-      $('#collective').fadeIn(800);
-  });
-
-  $('#next-web-1').click(function(){
-      $('#collective').fadeOut(400);
-      $('#web-1').fadeIn(800);
-  });
-
-  $('#next-intranet').click(function(){
-      $('#web-1').fadeOut(400);
-      $('#intranet').fadeIn(800);
-  });
-
-  $('#next-web-2').click(function(){
-      $('#intranet').fadeOut(400);
-      $('#web-2').fadeIn(800);
-  });
-//PREV
-
-  $('#prev-dropkick').click(function(){
-      $('#iceland').fadeOut(400);
-      $('#dropkick').fadeIn(800);
-  });
-
-  $('#prev-iceland').click(function(){
-      $('#collective').fadeOut(400);
-      $('#iceland').fadeIn(800);
-  });
-
-  $('#prev-collective').click(function(){
-      $('#web-1').fadeOut(400);
-      $('#collective').fadeIn(800);
-  });
-
-  $('#prev-web-1').click(function(){
-      $('#intranet').fadeOut(400);
-      $('#web-1').fadeIn(800);
-  });
-
-  $('#prev-intranet').click(function(){
-      $('#web-2').fadeOut(400);
-      $('#intranet').fadeIn(800);
-  });
 });
